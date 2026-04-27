ac286c8fbbc1474ffc44b56e5d6f210fb06c33da
n20186005-hash <n20186005@gmail.com>
2026-04-27 20:31:07 +0800

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https' as const, hostname: 'images.unsplash.com' },
+    ],
+  },
+};
+
+export default withNextIntl(nextConfig);
